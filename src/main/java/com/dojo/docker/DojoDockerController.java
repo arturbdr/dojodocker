30193561603d8b79e1dd819f77b8fee2d57b52e6
artur.drummond <artur.drummond@netshoes.com>
2017-03-31 18:15:36 -0300

    teste de docker log

--- src/main/java/com/dojo/docker/DojoDockerController.java
+++ src/main/java/com/dojo/docker/DojoDockerController.java
@@ -1,13 +1,26 @@
 package com.dojo.docker;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @RestController
+@Slf4j
 public class DojoDockerController {
 
     @RequestMapping("dojodocker")
     public String dojodocker() {
+
+
+        log.info("funcionando!");
         return "Dojo Docker Ok!";
+    }
+
+    @PostConstruct
+    public void logOk() {
+        log.debug("Log funcionando!");
     }
 }

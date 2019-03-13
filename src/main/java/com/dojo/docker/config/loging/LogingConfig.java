e1b274f65266ddbcd3dd39032de81eb5f7773feb
Artur Drummond <arturbdr@gmail.com>
2019-03-13 23:24:38 +0100

    - add: MDC to log all headers

---
+++ src/main/java/com/dojo/docker/config/loging/LogingConfig.java
@@ -0,0 +1,32 @@
+package com.dojo.docker.config.loging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class LogingConfig extends CommonsRequestLoggingFilter {
+
+    @Override
+    protected boolean shouldLog(HttpServletRequest request) {
+        return logger.isInfoEnabled();
+    }
+
+    @Override
+    protected void beforeRequest(HttpServletRequest request, String message) {
+        request.getHeaderNames().asIterator().forEachRemaining(headerName -> {
+            MDC.put(headerName, request.getHeader(headerName));
+        });
+
+        log.info("starting");
+    }
+
+    @Override
+    protected void afterRequest(HttpServletRequest request, String message) {
+        MDC.clear();
+    }
+}

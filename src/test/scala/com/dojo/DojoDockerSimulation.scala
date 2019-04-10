4776976f5002aff5769ead74ad6260bef6dfd763
Artur Drummond <arturbdr@gmail.com>
2019-04-10 17:29:47 +0200

    - fix: different users for stress testing

--- src/test/scala/com/dojo/DojoDockerSimulation.scala
+++ src/test/scala/com/dojo/DojoDockerSimulation.scala
@@ -14,8 +14,8 @@ import scala.concurrent.duration._
   * Running this stress test :
   *
   * LOCAL
-  * mvn clean gatling:test -Dgatling.simulationClass=com.dojo.DojoDockerSimulation -DtestEnvironment=LOCAL -DrampUsers=20 -DrampOverSeconds=60 -DmaxDurationSeconds=60 -Dgatling.charting.indicators.lowerBound=20 -Dgatling.charting.indicators.higherBound=40
-  * mvn clean gatling:test -Dgatling.simulationClass=com.dojo.DojoDockerSimulation -DtestEnvironment=INGRESS -DrampUsers=20 -DrampOverSeconds=60 -DmaxDurationSeconds=60 -Dgatling.charting.indicators.lowerBound=20 -Dgatling.charting.indicators.higherBound=40
+  * mvn clean gatling:test -Dgatling.simulationClass=com.dojo.DojoDockerSimulation -DtestEnvironment=LOCAL
+  * mvn clean gatling:test -Dgatling.simulationClass=com.dojo.DojoDockerSimulation -DtestEnvironment=INGRESS
   *
   *
   */
@@ -24,13 +24,10 @@ class DojoDockerSimulation extends Simulation {
 
   val environments = Map(
     "LOCAL" -> "http://localhost:8080",
-    "INGRESS" -> "https://192.168.99.100/ingress")
+    "INGRESS" -> "http://192.168.99.107/ingress/")
 
   val environment: String = Option(System.getProperty("testEnvironment")) getOrElse "LOCAL"
   val baseURL: String = Option(System.getProperty("baseURL")) getOrElse environments(environment)
-  val rampUsersEnv: String = Option(System.getProperty("rampUsers")) getOrElse "5"
-  val rampOverEnv: String = Option(System.getProperty("rampOverSeconds")) getOrElse "30"
-  val maxDurationSeconds: String = Option(System.getProperty("maxDurationSeconds")) getOrElse "30"
 
   val jsonFileFeeder: SourceFeederBuilder[Any] = jsonFile("gatling-user-feed.json").circular
 
@@ -50,17 +47,19 @@ class DojoDockerSimulation extends Simulation {
   var scnReqDojodockerslow: ScenarioBuilder = scenario("Get user with random delay time")
     .feed(jsonFileFeeder)
     .exec(reqDojodockerSlow)
-//    .exec { session =>
-//      println(session)
-//      session
-//    }
+  //    .exec { session =>
+  //      println(session)
+  //      session
+  //    }
 
 
   setUp(
     scnReqDojodockerslow.inject(
-      rampUsersPerSec(1) to rampUsersEnv.toInt during (rampOverEnv.toInt seconds)
+      incrementUsersPerSec(1)
+        .times(5)
+        .eachLevelLasting(10 seconds)
+        .startingFrom(1)
     ))
-    .assertions(global.responseTime.max.lt(300))
-    .maxDuration(maxDurationSeconds.toInt seconds)
+    .maxDuration(30 seconds)
     .protocols(httpConf)
 }

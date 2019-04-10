47db67113f667d764d004deb94f2754396ef613e
Artur Drummond <arturbdr@gmail.com>
2019-04-10 15:55:51 +0200

    - fix: the log was not properly configured

--- src/main/java/com/dojo/docker/config/log/LogConfig.java
+++ src/main/java/com/dojo/docker/config/log/LogConfig.java
@@ -3,29 +3,48 @@ package com.dojo.docker.config.log;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+import static org.springframework.util.StringUtils.hasText;
 
 @Slf4j
 @Component
-public class LogConfig extends CommonsRequestLoggingFilter {
+public class LogConfig extends OncePerRequestFilter {
+    private static final String X_REQUEST_ID = "x-request-id";
 
     @Override
-    protected boolean shouldLog(HttpServletRequest request) {
-        return logger.isInfoEnabled();
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        insertIntoMDC(request);
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            clearMDC();
+        }
     }
 
-    @Override
-    protected void beforeRequest(HttpServletRequest request, String message) {
-        request.getHeaderNames()
-                .asIterator()
-                .forEachRemaining(headerName -> MDC.put(headerName, request.getHeader(headerName)));
-        log.info("starting");
+    private void insertIntoMDC(final HttpServletRequest request) {
+
+        final String xRequestIdValue = getXRequestId(request);
+        MDC.put(X_REQUEST_ID, xRequestIdValue);
+
     }
 
-    @Override
-    protected void afterRequest(HttpServletRequest request, String message) {
-        MDC.clear();
+    private void clearMDC() {
+        MDC.remove(X_REQUEST_ID);
+    }
+
+    private String getXRequestId(final HttpServletRequest request) {
+        String xRequestIdValue = request.getHeader(X_REQUEST_ID);
+        if (!hasText(xRequestIdValue)) {
+            xRequestIdValue = UUID.randomUUID().toString();
+        }
+        return xRequestIdValue;
     }
 }
